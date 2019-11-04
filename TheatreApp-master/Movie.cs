b389f108d61ed7dfdd4687d0d877bd18cb955671
jaikunwarsidhu <Jaikunwarsidhu@gmail.com>
2019-11-04 18:07:00 -0500

    Initial commit

---
+++ TheatreApp-master/Movie.cs
@@ -0,0 +1,33 @@
+﻿using System;
+namespace TheatreApp
+{
+    public class Movie
+    {
+        public int Length { get;}
+        public int Year { get;}
+        public string Title { get;}
+        public Genre Genre { get; private set;}
+        public List<string> Cast { get;}
+
+        public Movie(string name, int year, int length)
+        {
+            Name = name;
+            Year = year;
+            Length = length;
+            Cast = new List<string>();
+        }
+        public void AddActor(string actor)
+        {
+            Cast.Add(actor);
+        }
+        public void SetGenre(Genre genre)
+        {
+            Genre = genre;
+        }
+        public override string ToString()
+        {
+            return $"{Title} {Year} \n{string.Join(",", Cast)} \nLength:{Length}min";
+        }
+
+     }
+}
---
+++ TheatreApp-master/Show.cs
@@ -0,0 +1,23 @@
+﻿using System;
+namespace TheatreApp
+{
+    public class Show
+    {
+       public double Price { get; }
+        public MovieDay Day { get; }
+        public Movie Movie { get; }
+        public Time Time { get; }
+        public Show(Movie movie, MovieDay day, double price, Time time)
+        {
+            Movie = movie;
+            Day = day;
+            Price = price;
+            Time = time;
+        }
+        public override string ToString()
+        {
+
+            return $" The {Movie}start at {Day} {Time} and cost {Price}";
+        }
+    }
+}
